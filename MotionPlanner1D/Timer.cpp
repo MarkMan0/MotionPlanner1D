b5918802c3df949f8c55aeb88517f275617ce628
MarkMan0 <pal.mark28@gmail.com>
2020-12-29 21:19:44 +0100

    Initial, create Timer class with tests

---
+++ MotionPlanner1D/Timer.cpp
@@ -0,0 +1,59 @@
+#include "Timer.h"
+
+static void default_callback (void) {
+    return;
+}
+
+Timer::Timer () : callback_ (default_callback) { }
+
+Timer::Timer (callback_t cb) : callback_ (cb) { }
+
+void Timer::tick () {
+    ++prescale_cnt_;
+    if (prescale_cnt_ >= prescale_val_) {
+        prev_counter_ = counter_;
+        ++counter_;
+        prescale_cnt_ = 0;
+    }
+
+    if (counter_ >= threshold_ || counter_ < prev_counter_) {
+        counter_ = 0;
+        callback_ ();
+    }
+}
+
+void Timer::setPrescale (unsigned int v) {
+    prescale_val_ = v;
+}
+
+void Timer::setThreshold (counter_t v) {
+    threshold_ = v;
+}
+
+void Timer::enable (uint32_t freq) {
+    enabled_ = true;
+}
+void Timer::disable () {
+    enabled_ = false;
+}
+
+unsigned int Timer::getPrescale () const {
+    return prescale_val_;
+}
+
+Timer::counter_t Timer::getThreshold () const {
+    return threshold_;
+}
+
+Timer::counter_t Timer::getCounter () const {
+    return counter_;
+}
+
+void Timer::set_callback (callback_t cb) {
+    callback_ = cb;
+}
+
+void Timer::reset () {
+    counter_ = 0;
+    prescale_cnt_ = 0;
+}
---
+++ MotionPlanner1D/Timer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <functional>
+#include <limits>
+#include <cstdint>
+
+class Timer {
+
+public:
+    using callback_t = void (*)(void);
+    using counter_t = uint32_t;
+
+    Timer ();
+    Timer (callback_t);
+    void tick ();
+
+    void setPrescale (unsigned int);
+    void setThreshold (counter_t);
+
+    void enable (uint32_t freq = 0);
+    void disable ();
+
+    unsigned int getPrescale () const;
+    counter_t getThreshold () const;
+    counter_t getCounter () const;
+
+    void set_callback (callback_t cb);
+
+    void reset ();
+
+private:
+    callback_t callback_;
+
+    bool enabled_ = false;
+
+    counter_t threshold_ = std::numeric_limits<counter_t>::max ();
+    counter_t counter_ = 0;
+    counter_t prev_counter_ = 0;
+    unsigned int prescale_val_ = 0;;
+    unsigned int prescale_cnt_ = 0;
+
+};
